44d105888d3adf21ba804e4a4a2e5f45fdb9e807
Ivan Kalinin <mufnz1@gmail.com>
2025-01-20 15:09:43 +0700

    upd ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,1 +1,14 @@
- 
+module.exports = {
+  apps: [{
+    name: "CryptoNewsParser",
+    script: "./dist/index.js",
+    env: {
+      NODE_ENV: "production",
+    },
+    watch: false,
+    instances: 1,
+    autorestart: true,
+    max_memory_restart: "1G",
+    env_file: ".env"
+  }]
+} 
